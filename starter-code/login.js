bed2f13b0ed9d135cd6d3a27c014521a23a94608
yasuper8 <ysuper8@gmail.com>
2016-10-26 12:31:04 -0700

    added login.js and security_question.js

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,12 @@
 console.log("login.js loaded");
+
+var userLogin = {
+  userName: "yasu",
+  userPassword: "123456"
+}
+
+var userAnswer = prompt("Please enter password for the user " + userLogin.userName);
+
+while (prompt("Please enter password for the user " + userLogin.userName) !== userLogin.userPassword) {
+  prompt("Please enter password for the user " + userLogin.userName)
+}
--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,26 @@
 console.log("security_questions.js loaded");
+
+var securityQuestions = [
+  {
+    questions: "Where is the capital city in Japan?",
+    expectedAanswer: "Tokyo"
+  },
+  {
+    questions: "How many presidents has the US had?",
+    expectedAanswer: "43"
+  },
+  {
+    questions: "How many states are in the US?",
+    expectedAanswer: "50"
+  }
+]
+
+var userAnswer;
+
+for (var i = securityQuestions.length - 1; i >= 0; i--) {
+  userAnswer = prompt(securityQuestions[i].questions);
+  if(userAnswer !== securityQuestions[i].expectedAanswer) {
+    alert("That's not a correct answer :( ");
+    break;
+  }
+}
